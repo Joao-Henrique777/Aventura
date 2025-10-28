859547da70b98db8074434d2a2e5d4b004bea6d6
Joao-Henrique777 <joao.baptista.reis@escola.pr.gov.br>
2025-10-28 08:46:15 -0300

    Fase inicial, contem varios erros e a sintaxe não parece estar certa.

---
+++ script.js
@@ -0,0 +1,13 @@
+const proximo = document.querySelectorAll('.btn-proximo')
+
+data_atual = "passo_ativo"
+
+proximo.forEach(button => {
+    button.addEventListener('click',function(){
+        const Atual = document.querySelector('.ativo')
+        const Proxima_Etapa = data_id
+
+
+    })
+
+})
