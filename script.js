f2cabe54987b0c3124e66416dea4e5f2c9bb22ad
Joao-Henrique777 <joao.baptista.reis@escola.pr.gov.br>
2025-11-18 08:41:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const proximo = document.querySelectorAll('.btn-proximo')
 
-data_atual = "passo_ativo"
-
 proximo.forEach(button => {
-    button.addEventListener('click',function(){
-        const Atual = document.querySelector('.passo ativo')
+    button.addEventListener('click', function(){
+        const Atual = document.querySelector('.ativo')
         const Proxima_Etapa = 'passo-' + this.getAttribute('data-proximo')
         const Removerpasso = removeEventListener(passo(id))
 
